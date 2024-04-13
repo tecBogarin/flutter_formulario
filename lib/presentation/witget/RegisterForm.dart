afa9bd17dd985dbb3a9f665e7c16a7bb759a0b15
Jose Ramon Bogarin Valenzuela <tec.jrbogarin@gmail.com>
2024-04-12 23:12:23 -0700

    segundo cambio

--- lib/presentation/witget/RegisterForm.dart
+++ lib/presentation/witget/RegisterForm.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:uso_de_formularios/presentation/witget/input/custom_text_form_field.dart';
 
-class RegisterForm extends StatelessWidget {
+class RegisterForm extends StatefulWidget {
   const RegisterForm({super.key});
 
+  @override
+  State<RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
         child: Column(
       children: [
+        const CustomTextFormField(),
+        const SizedBox(
+          height: 20,
+        ),
+        const CustomTextFormField(),
+        const SizedBox(
+          height: 20,
+        ),
         const CustomTextFormField(),
         const SizedBox(
           height: 20,
--- lib/presentation/witget/input/custom_text_form_field.dart
+++ lib/presentation/witget/input/custom_text_form_field.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validador;
+  final bool obscureText;
+
+  const CustomTextFormField(
+      {super.key,
+      this.label,
+      this.hint,
+      this.errorMessage,
+      this.onChanged,
+      this.validador,
+      this.obscureText = false});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    final colors = Theme.of(context).colorScheme;
+    final border = OutlineInputBorder(borderRadius: BorderRadius.circular(40));
+    return TextFormField(
+      onChanged: onChanged,
+      validator: validador,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+          enabledBorder: border,
+          focusedBorder:
+              border.copyWith(borderSide: BorderSide(color: colors.primary)),
+          errorBorder: border.copyWith(
+              borderSide: BorderSide(color: Colors.red.shade800)),
+          focusedErrorBorder: border.copyWith(
+              borderSide: BorderSide(color: Colors.red.shade800)),
+          isDense: true,
+          label: label != null ? Text(label!) : null,
+          hintText: hint,
+          errorText: errorMessage,
+          focusColor: colors.primary),
+    );
   }
 }
